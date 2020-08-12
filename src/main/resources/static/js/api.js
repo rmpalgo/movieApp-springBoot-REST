552eb1696e1b6191a12e2bd3fed0df0360256b3a
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-08-11 21:59:30 -0500

    feat: transferred over the js files from the movie application project.

---
+++ src/main/resources/static/js/api.js
@@ -0,0 +1,33 @@
+const api = {
+
+    getMovies: () => {
+        return fetch('/api/movies')
+            .then(response => response.json());
+    },
+
+    addMovie: (movieObj) => {
+        return fetch('/api/movies', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(movieObj),
+        })
+            .then( response => response.json() )
+            .catch( error => console.log(error));
+    },
+    editMovie: (movieObj, id) => {
+        return fetch(`/api/movies/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(movieObj),
+        });
+    },
+
+    getOMBDData: (addMovieTitle) => {
+        return fetch(`http://www.omdbapi.com?s=${addMovieTitle}&apikey=${ombd_key}`)
+            .then( response => response.json() );
+    }
+};
