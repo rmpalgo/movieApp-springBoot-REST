9af49034ad6ee7007d8ff2dc099f0264e2489561
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-08-11 22:35:05 -0500

    feat: created rest mapping for create and get movies.

--- src/main/java/com/movieapp/movieapp/controllers/MovieController.java
+++ src/main/java/com/movieapp/movieapp/controllers/MovieController.java
@@ -1,12 +1,31 @@
 package com.movieapp.movieapp.controllers;
 
 import com.movieapp.movieapp.model.Movie;
+import com.movieapp.movieapp.services.MovieService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MovieController {
 
+    private MovieService movieService;
 
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @PostMapping("/api/movies")
+    public Movie addMovie(@RequestBody Movie movie) {
+        return movieService.saveMovie(movie);
+    }
+
+    @GetMapping("/api/movies")
+    public List<Movie> getAllMovies() {
+        return movieService.getMovies();
+    }
 
 }
